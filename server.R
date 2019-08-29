a6e6e90afed7d8213e920d49e54e28f7c29253c5
Amit Prasad <amitprasad531@gmail.com>
2019-08-29 03:32:39 +0200

    Added changes to database section with PickerInput and latest available data

--- server.R
+++ server.R
@@ -1,5 +1,7 @@
 shinyServer(function(input, output, session) {
 
+source("server/correlations.R", local = TRUE) 
+  
 source("server/trends.R", local = TRUE)  
 
 source("server/maps.R", local = TRUE)
---
+++ server/correlations.R
@@ -0,0 +1,91 @@
+###
+### Scatter Plot Start
+###
+
+temp_corr <- reactive({
+  req(input$region_corr, input$country_corr)
+  gpw_or %>%
+    filter(Region %in% input$region_corr)
+})  
+
+observe({
+  req(input$country_corr)
+  updateSelectizeInput(session,
+                       "country_corr", "Select Countries",
+                       choices = c("All", unique(as.character(temp_corr()$Country))),
+                       selected = input$country_corr)
+})
+
+cc <- eventReactive(input$create_corr, {
+  as.character(input$country_corr)
+}, ignoreNULL = FALSE)
+
+rc <- eventReactive(input$create_corr, {
+  as.character(input$region_corr)
+}, ignoreNULL = FALSE)
+
+corr_data <- reactive({
+  req(input$xvar_corr, input$yvar_corr)
+  c <- cc()
+  r <- rc()
+  trend_corr <- gpw_or %>% filter(Region %in% r)
+  if (!"All" %in% c) {
+    trend_corr <- trend_corr %>%
+      filter(Country %in% c) 
+  }
+  xvar <- trend_corr %>% filter(Year == input$xyear_corr) %>% select(input$xvar_corr)
+  yvar <- trend_corr %>% filter(Year == input$yyear_corr) %>% select(input$yvar_corr)
+  trend_corr <- trend_corr %>%
+    filter(Year == 2017) %>%
+    select(Country, Region, ISO3)
+  trend_corr <- cbind(trend_corr, xvar[[input$xvar_corr]], yvar[[input$yvar_corr]])
+  names(trend_corr)[4:5] <- c("xvar", "yvar")
+  trend_corr
+})
+
+observeEvent(corr_data(), {
+  output$correlations <- renderPlotly({
+    label_index_x <- match(input$xvar_corr, gpw_ind)
+    label_index_y <- match(input$yvar_corr, gpw_ind)
+    y_label <- str_wrap(ind_label(label_index_y), width = 80)
+    if (all(is.na(corr_data()$xvar)) | all(is.na(corr_data()$yvar))) {
+      df <- data.frame(year = c(2000:2018), y = c(1:28))
+      x <- ggplot(df, aes(x = year, y = y)) +
+        geom_abline(intercept = 0, slope = 1, linetype=3, col="black") +
+        labs(x = ind_label(label_index_x), y = y_label,  title = "Data for at least one indicator is not available.") +
+        theme(rect = element_rect(fill = "transparent"),
+              plot.background = element_rect(fill = "transparent", color = NA),
+              text = element_text(family = "Arial"),
+              axis.title = element_text(size = 14),
+              axis.text = element_text(size = 13),
+              plot.margin=unit(c(0,0,0,1.2),"cm"),
+              title = element_text(size = 16))
+      ggplotly(x)
+    } else {
+      med_x <- median(corr_data()$xvar, na.rm = TRUE)
+      med_y <- median(corr_data()$yvar, na.rm = TRUE)
+      min_x <- min(corr_data()$xvar, na.rm = TRUE)
+      min_y <- min(corr_data()$yvar, na.rm = TRUE)
+      g <- ggplot(corr_data(), aes(x = xvar, y = yvar, fill = Region, col=Region)) +
+        geom_point(shape=21, alpha=0.8, stroke = 0, size = 4,
+                   aes(text=sprintf("Country: %s<br>xVar: %s<br>yVar: %s", ISO3, xvar, yvar))) +
+        labs(x = paste(ind_label(label_index_x), " (", input$xyear_corr, ")", sep=""), 
+             y = paste(y_label, " (", input$yyear_corr, ")", sep="")) +
+        geom_hline(yintercept = med_y, linetype = 3, size = 0.5) +
+        geom_vline(xintercept = med_x, linetype = 3, size = 0.5) +
+        annotate("text", x = med_x, y = min_y, label = "X-axis median", size=5) +
+        annotate("text", x = min_x, y = med_y+0.02*med_y, label = "Y-axis median", size=5) +
+        scale_fill_manual(values=c('AFRO' = '#fda09a', 'EMRO'='#cdbc64', 'EURO'='#36c14c', 
+                                   'PAHO' = '#00bfc3', 'SEARO' = '#92baff', 'WPRO' = '#fa95eb')) +
+        theme(plot.background = element_rect(fill = "transparent", color = NA),
+              panel.background = element_rect(fill = "white"),
+              panel.grid.major = element_line(color = "lightgrey"),
+              text = element_text(family = "Arial"),
+              axis.title = element_text(size = 12),
+              plot.margin=unit(c(0,0,0,1.2),"cm"),
+              axis.text = element_text(size = 10))
+      ggplotly(g, tooltip="text") %>% layout(showlegend = TRUE, legend = list(font = list(size = 10)))
+    }
+  })
+}, ignoreNULL = FALSE)
+
